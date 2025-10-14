c1f79d38bd7c7b76e1a67270106e392b860d8c29
Kaleb <kaleb.starr@proton.me>
2025-10-14 16:44:54 +0200

    Intiial commit

---
+++ flake.nix
@@ -0,0 +1,30 @@
+{
+
+  description = "NixOS ThinkPad";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = { nixpkgs, home-manager, ... }: {
+    nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager {
+          home-manager = {
+            useGlobalPkgs = true;
+            useUserPackages = true;
+            users.kaleb = import ./home.nix;
+          };
+        }
+      ];
+    };
+  };
+
+}
---
+++ home.nix
@@ -0,0 +1,51 @@
+{ config, pkgs, ... }:
+
+{
+
+  home.username = "kaleb";
+  home.homeDirectory = "/home/kaleb";
+
+  home.stateVersion = "25.05";
+
+  home.packages = with pkgs; [
+    neovim
+    htop
+    fastfetch
+  ];
+
+  programs.git = {
+    enable = true;
+    userName = "Kaleb";
+    userEmail = "kaleb.starr@proton.me";
+  };
+
+  wayland.windowManager.hyprland = {
+    enable = true;
+    settings = {
+      monitor = ",preferred,auto,1";
+
+      exec-once = "waybar";
+
+      input = {
+        kb_layout = "de";
+        follow_mouse = 1;
+      };
+
+      general = {
+        gaps_in = 5;
+        gaps_out = 10;
+        border_size = 2;
+        "col.active_border" = "rgba(cdd6f4aa) rgba(b4befeaa) 45deg";
+        "col.inactive_border" = "rgba(595959aa)";
+        layout = "dwindle";
+      };
+    };
+  };
+
+  programs.waybar = {
+    enable = true;
+  };
+
+  programs.home-manager.enable = true;
+
+}
