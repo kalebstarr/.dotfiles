7d593ae86665421c6335ea3844e726ed181b091c
Kaleb <kaleb.starr@proton.me>
2025-10-14 19:04:05 +0200

    Add fonts

--- home.nix
+++ home.nix
@@ -18,6 +18,12 @@
     htop
     fastfetch
     brave
+
+    noto-fonts
+    noto-fonts-cjk-sans
+    noto-fonts-emoji
+    nerd-fonts.fira-code
+    nerd-fonts.jetbrains-mono
   ];
 
   programs.git = {
