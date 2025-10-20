1c6ed42e3d5b4a255df1f7c7d3f795d1bb007332
Kaleb <kaleb.starr@proton.me>
2025-10-20 19:15:45 +0200

    Add nixvim options

---
+++ nixvim/options.nix
@@ -0,0 +1,22 @@
+{ ... }:
+
+{
+
+  programs.nixvim = {
+    globals = {
+      mapleader = " ";
+      maplocalleader = "\\";
+    };
+
+    opts = {
+      expandtab = true;
+      shiftwidth = 4;
+      tabstop = 4;
+      softtabstop = 4;
+      number = true;
+      relativenumber = true;
+      termguicolors = true;
+    };
+  };
+
+}
