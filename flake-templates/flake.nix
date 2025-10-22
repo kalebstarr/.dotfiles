fbfcc22f57c0436cf74354779e963653c05391e8
Kaleb <kaleb.starr@proton.me>
2025-10-22 19:42:27 +0200

    Add template development flake

---
+++ flake-templates/flake.nix
@@ -0,0 +1,40 @@
+{
+  description = "Development environment";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    flake-utils = {
+      url = "github:numtide/flake-utils";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+      ...
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            (pkgs.python314.withPackages (
+              pypkgs: with pypkgs; [
+                numpy
+                pandas
+              ]
+            ))
+            cargo
+            nodejs
+            gcc
+          ];
+        };
+      }
+    );
+}
