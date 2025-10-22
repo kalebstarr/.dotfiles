e3356971d22d56d1ebcd3ecf57fae081bc8e5d42
Kaleb <kaleb.starr@proton.me>
2025-10-22 21:14:55 +0200

    Update template flake

--- flake-templates/flake.nix
+++ flake-templates/flake.nix
@@ -3,10 +3,7 @@
 
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
-    flake-utils = {
-      url = "github:numtide/flake-utils";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
+    flake-utils.url = "github:numtide/flake-utils";
   };
 
   outputs =
