d79b764d531fb1437e636b1250bb64acdacf9f02
Kaleb <kaleb.starr@proton.me>
2025-11-10 00:29:57 +0100

    Add zig formatter

--- nixvim/plugins/formatters.nix
+++ nixvim/plugins/formatters.nix
@@ -14,6 +14,7 @@
             html = [ "prettier" ];
             css = [ "prettier" ];
             javascript = [ "prettier" ];
+            zig = [ "zigfmt" ];
           };
         };
       };
