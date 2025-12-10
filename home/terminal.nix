9ef58bdf3350cb02ab3800a43606a79123080610
Kaleb <kaleb.starr@proton.me>
2025-12-10 20:31:58 +0100

    Add ghostty config

--- home/terminal.nix
+++ home/terminal.nix
@@ -2,29 +2,45 @@
 
 {
 
-  programs.foot = {
-    enable = true;
-    settings = {
-      main = {
-        term = "xterm-256color";
-        font = "FiraCode Nerd Font";
-        dpi-aware = "yes";
-        pad = "10x10";
+  programs = {
+    foot = {
+      enable = true;
+      settings = {
+        main = {
+          term = "xterm-256color";
+          font = "FiraCode Nerd Font";
+          dpi-aware = "yes";
+          pad = "10x10";
+        };
+        cursor = {
+          style = "beam";
+          blink = "yes";
+        };
+        colors = {
+          background = "1e1e2e";
+          foreground = "cdd6f4";
+        };
+        bell = {
+          urgent = "no";
+          notify = "no";
+        };
+        scrollback = {
+          lines = 10000;
+        };
       };
-      cursor = {
-        style = "beam";
-        blink = "yes";
-      };
-      colors = {
+    };
+
+    ghostty = {
+      enable = true;
+      enableZshIntegration = true;
+      settings = {
+        font-family = "FiraCode Nerd Font";
+        font-size = 12;
+        window-padding-x = 10;
+        window-padding-y = 10;
+        cursor-style = "block";
         background = "1e1e2e";
         foreground = "cdd6f4";
-      };
-      bell = {
-        urgent = "no";
-        notify = "no";
-      };
-      scrollback = {
-        lines = 10000;
       };
     };
   };
