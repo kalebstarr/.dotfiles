6c74ac5c4e8bd4ab4bcfe3cee2f42adfb154f773
Kaleb <kaleb.starr@proton.me>
2026-01-04 22:02:17 +1300

    Add stylix config

--- home/hyprland.nix
+++ home/hyprland.nix
@@ -83,13 +83,40 @@
   stylix = {
     enable = true;
     image = ../wallpapers/Sunset.png;
-    base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
+    # base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
     polarity = "dark";
+
+    targets = {
+      gtk.enable = true;
+      gnome.enable = true;
+    };
 
     cursor = {
       package = pkgs.bibata-cursors;
       name = "Bibata-Modern-Classic";
       size = 24;
+    };
+
+    fonts = {
+      monospace = {
+        package = pkgs.nerd-fonts.fira-code;
+        name = "FiraCode Nerd Font";
+      };
+      sansSerif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Sans";
+      };
+      serif = {
+        package = pkgs.dejavu_fonts;
+        name = "DejaVu Serif";
+      };
+
+      sizes = {
+        applications = 12;
+        terminal = 12;
+        desktop = 10;
+        popups = 10;
+      };
     };
   };
 
