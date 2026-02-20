48073a79a0258b42539e234cf087c29461c25c98
Kaleb <kaleb.starr@proton.me>
2026-02-20 02:08:11 +0100

    Add starship config

--- home/sh.nix
+++ home/sh.nix
@@ -2,7 +2,49 @@
 
 {
 
-  programs.starship.enable = true;
+  programs.starship = {
+    enable = true;
+    settings = {
+      format = "$directory$git_branch$git_status$nix_shell$python$nodejs$rust$golang$cmd_duration$line_break$character";
+
+      character = {
+        success_symbol = "[❯](bold green)";
+        error_symbol = "[❯](bold red)";
+      };
+
+      directory = {
+        truncation_length = 3;
+        truncate_to_repo = true;
+      };
+
+      git_branch = {
+        format = "[$branch]($style) ";
+      };
+
+      git_status = {
+        format = "[$all_status$ahead_behind]($style) ";
+        conflicted = "!";
+        ahead = "⇡";
+        behind = "⇣";
+        diverged = "⇕";
+        untracked = "?";
+        stashed = "";
+        modified = "~";
+        staged = "+";
+        renamed = "»";
+        deleted = "✘";
+      };
+
+      cmd_duration = {
+        min_time = 2000;
+        format = "[$duration]($style) ";
+      };
+
+      nix_shell = {
+        format = "[nix]($style) ";
+      };
+    };
+  };
 
   programs.zsh = {
     enable = true;
