2a884f2dc607057804847df410163024a2f3fa3f
Kaleb <kaleb.starr@proton.me>
2026-03-24 03:57:11 +0100

    Add theming to emacs

--- home-modules/doom-emacs/doom.d/config.el
+++ home-modules/doom-emacs/doom.d/config.el
@@ -32,7 +32,9 @@
 ;; There are two ways to load a theme. Both assume the theme is installed and
 ;; available. You can either set `doom-theme' or manually load a theme with the
 ;; `load-theme' function. This is the default:
-(setq doom-theme 'doom-one)
+(setq doom-theme 'doom-tokyo-night)
+
+(setq doom-font (font-spec :family "FiraCode Nerd Font" :size 12))
 
 ;; This determines the style of line numbers in effect. If set to `nil', line
 ;; numbers are disabled. For relative line numbers, set this to `relative'.
