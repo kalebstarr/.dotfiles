91753b0a9466704d97c108d7c5d8e08631b1b0f3
Kaleb <kaleb.starr@proton.me>
2026-05-31 23:38:55 +0200

    Remove unused lsp and add lua

--- home-modules/nixvim/plugins/lsp-config.nix
+++ home-modules/nixvim/plugins/lsp-config.nix
@@ -16,8 +16,7 @@
         };
         ts_ls.enable = true;
         elixirls.enable = true;
-        gopls.enable = true;
-        clojure.enable = true;
+        lua_ls.enable = true;
       };
       keymaps = {
         lspBuf = {
