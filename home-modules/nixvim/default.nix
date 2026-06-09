959c8e1ec80fbbb6bf9eaf9c61338f72f1ed5405
Kaleb <kaleb.starr@proton.me>
2026-06-10 00:23:16 +0200

    Remove nixvim colorscheme

--- home-modules/nixvim/default.nix
+++ home-modules/nixvim/default.nix
@@ -24,13 +24,6 @@
   programs.nixvim = {
     enable = true;
 
-    colorschemes.catppuccin = {
-      enable = true;
-      settings = {
-        flavour = "mocha";
-      };
-    };
-
     plugins = {
       web-devicons.enable = true;
     };
