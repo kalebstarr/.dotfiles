a3fd28b5ff225151f2cb2b709ca151baeec8d5ad
Kaleb <kaleb.starr@proton.me>
2026-06-11 13:41:09 +0200

    Update noctalia url

--- flake.nix
+++ flake.nix
@@ -33,7 +33,7 @@
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
     noctalia = {
-      url = "github:noctalia-dev/noctalia-shell";
+      url = "github:noctalia-dev/noctalia/legacy-v4";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
