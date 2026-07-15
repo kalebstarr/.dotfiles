a0519e6fbd11a8eec26bc94980539d5c4f9cebb1
Kaleb <kaleb.starr@proton.me>
2026-07-15 20:55:51 +0200

    refactor(wsl): remove obsolete systemctl shim

--- hosts/wsl/default.nix
+++ hosts/wsl/default.nix
@@ -1,17 +1,7 @@
-# Edit this configuration file to define what should be installed on
-# your system. Help is available in the configuration.nix(5) man page, on
-# https://search.nixos.org/options and in the NixOS manual (`nixos-help`).
-
 # NixOS-WSL specific options are documented on the NixOS-WSL repository:
 # https://github.com/nix-community/NixOS-WSL
 
-{
-  config,
-  lib,
-  pkgs,
-  inputs,
-  ...
-}:
+{ inputs, ... }:
 
 {
   imports = [
@@ -26,18 +16,7 @@
 
   services.openssh.enable = false;
 
-  system.activationScripts.binSystemctl = {
-    text = "ln -sfn /run/current-system/sw/bin/systemctl /usr/bin/systemctl";
-    deps = [ ];
-  };
-
   programs.dconf.enable = true;
 
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It's perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
-  system.stateVersion = "25.05"; # Did you read the comment?
+  system.stateVersion = "25.05";
 }
