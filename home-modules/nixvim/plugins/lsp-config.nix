430a19cbf2a15a7aa3d319839c332fc1c227040b
Kaleb <kaleb.starr@proton.me>
2026-07-15 19:14:17 +0200

    Update lsp-ocnfig

--- home-modules/nixvim/plugins/lsp-config.nix
+++ home-modules/nixvim/plugins/lsp-config.nix
@@ -1,34 +1,72 @@
 {
 
-  programs.nixvim = {
-    plugins.lsp = {
-      enable = true;
-      servers = {
-        nixd.enable = true;
-        clangd.enable = true;
-        zls.enable = true;
-        ruff.enable = true;
-        pyright.enable = true;
-        rust_analyzer = {
-          enable = true;
-          installCargo = false;
-          installRustc = false;
-        };
-        ts_ls.enable = true;
-        elixirls.enable = true;
-        lua_ls.enable = true;
-        jdtls.enable = true;
-      };
-      keymaps = {
-        lspBuf = {
-          gd = "definition";
-          K = "hover";
-          "<leader>ca" = "code_action";
-        };
+  programs.nixvim.plugins.lsp = {
+    enable = true;
 
-        diagnostic = {
-          "<leader>e" = "open_float";
-        };
+    servers = {
+      nixd = {
+        enable = true;
+        packageFallback = true;
+      };
+
+      clangd = {
+        enable = true;
+        packageFallback = true;
+      };
+
+      zls = {
+        enable = true;
+        packageFallback = true;
+      };
+
+      ruff = {
+        enable = true;
+        packageFallback = true;
+      };
+
+      pyright = {
+        enable = true;
+        packageFallback = true;
+      };
+
+      rust_analyzer = {
+        enable = true;
+        packageFallback = true;
+
+        installCargo = false;
+        installRustc = false;
+      };
+
+      ts_ls = {
+        enable = true;
+        packageFallback = true;
+      };
+
+      elixirls = {
+        enable = true;
+        packageFallback = true;
+      };
+
+      lua_ls = {
+        enable = true;
+        packageFallback = true;
+      };
+
+      jdtls = {
+        enable = true;
+        packageFallback = true;
+      };
+    };
+
+    keymaps = {
+      lspBuf = {
+        gd = "definition";
+        K = "hover";
+        "<leader>ca" = "code_action";
+      };
+
+      diagnostic = {
+        "<leader>e" = "open_float";
       };
     };
   };
