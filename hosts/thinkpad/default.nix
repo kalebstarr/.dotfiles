f3de6a03f27dab86290dddbb3404d868299bb726
Kaleb <kaleb.starr@proton.me>
2026-09-15 00:43:37 +0200

    Switch to standalone DankGreeter

--- hosts/thinkpad/default.nix
+++ hosts/thinkpad/default.nix
@@ -2,6 +2,7 @@
 
 {
   imports = [
+    inputs.dank-greeter.nixosModules.default
     ./hardware-configuration.nix
     ./laptop.nix
     ./desktop.nix
@@ -27,14 +28,12 @@
     xkb.layout = "de";
   };
 
-  services.displayManager = {
-    defaultSession = "niri";
-    dms-greeter = {
-      enable = true;
-      package = inputs.dms.packages.${pkgs.stdenv.hostPlatform.system}.dms-shell;
-      compositor.name = "niri";
-      configHome = "/home/kaleb";
-    };
+  services.displayManager.defaultSession = "niri";
+
+  programs.dms-greeter = {
+    enable = true;
+    compositor.name = "niri";
+    configHome = "/home/kaleb";
   };
 
   environment.systemPackages = with pkgs; [
